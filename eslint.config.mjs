d50746f6605713ce0674f9483ea5cd6b12c4c87d
Morcinus <11.honzik@gmail.com>
2025-10-20 17:16:19 +0200

    feat: :sparkles: Add eslint, prettier and husky

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+// Flat ESLint config (ESM) for ESLint v9
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import storybookPlugin from "eslint-plugin-storybook";
+import prettierConfig from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      ".yarn/**",
+      "storybook-static/**",
+    ],
+  },
+  {
+    files: ["**/*.{ts,tsx,js,jsx,mjs,cjs}"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { React: "readonly" },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      storybook: storybookPlugin,
+    },
+    settings: { react: { version: "detect" } },
+    rules: {
+      curly: "error",
+      eqeqeq: "error",
+      "no-useless-return": "error",
+      "no-var": "error",
+      "operator-assignment": "error",
+      "prefer-const": "error",
+      "prefer-template": "error",
+      yoda: "error",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    files: ["**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+    rules: { "storybook/hierarchy-separator": "error" },
+  },
+  prettierConfig,
+];
