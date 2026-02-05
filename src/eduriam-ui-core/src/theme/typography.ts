ecdeb3a6190d8238c0c14fa795cea7a33dfd23c1
Morcinus <11.honzik@gmail.com>
2026-02-05 16:28:27 +0100

    feat: :sparkles: Adjust typography line heights

--- src/eduriam-ui-core/src/theme/typography.ts
+++ src/eduriam-ui-core/src/theme/typography.ts
@@ -25,23 +25,78 @@ const responsiveFontSize = (mobile: number, desktop: number) => ({
   [baseTheme.breakpoints.up("md")]: { fontSize: desktop },
 });
 
+const LINE_HEIGHT_RATIO = 1.21;
+
 export const coreTypography: TypographyOptions = {
   fontFamily: ["Inter", "sans-serif"].join(","),
-  h1: { ...responsiveFontSize(48, 60), fontWeight: 700 },
-  h2: { ...responsiveFontSize(40, 48), fontWeight: 700 },
-  h3: { ...responsiveFontSize(34, 40), fontWeight: 700 },
-  h4: { ...responsiveFontSize(28, 32), fontWeight: 600 },
-  h5: { ...responsiveFontSize(24, 26), fontWeight: 600 },
-  h6: { ...responsiveFontSize(20, 20), fontWeight: 600 },
-  subtitle1: { ...responsiveFontSize(18, 18), fontWeight: 400 },
-  subtitle2: { ...responsiveFontSize(16, 16), fontWeight: 600 },
-  body1: { ...responsiveFontSize(16, 16), fontWeight: 400 },
-  body2: { ...responsiveFontSize(14, 14), fontWeight: 400 },
-  button: { ...responsiveFontSize(16, 16), fontWeight: 700 },
-  caption: { ...responsiveFontSize(12, 12), fontWeight: 400 },
-  overline: { ...responsiveFontSize(10, 10), fontWeight: 400 },
+  h1: {
+    ...responsiveFontSize(48, 60),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 700,
+  },
+  h2: {
+    ...responsiveFontSize(40, 48),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 700,
+  },
+  h3: {
+    ...responsiveFontSize(34, 40),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 700,
+  },
+  h4: {
+    ...responsiveFontSize(28, 32),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 600,
+  },
+  h5: {
+    ...responsiveFontSize(24, 26),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 600,
+  },
+  h6: {
+    ...responsiveFontSize(20, 20),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 600,
+  },
+  subtitle1: {
+    ...responsiveFontSize(18, 18),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 400,
+  },
+  subtitle2: {
+    ...responsiveFontSize(16, 16),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 600,
+  },
+  body1: {
+    ...responsiveFontSize(16, 16),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 400,
+  },
+  body2: {
+    ...responsiveFontSize(14, 14),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 400,
+  },
+  button: {
+    ...responsiveFontSize(16, 16),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 700,
+  },
+  caption: {
+    ...responsiveFontSize(12, 12),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 400,
+  },
+  overline: {
+    ...responsiveFontSize(10, 10),
+    lineHeight: LINE_HEIGHT_RATIO,
+    fontWeight: 400,
+  },
   fieldLabel: {
     ...responsiveFontSize(18, 18),
+    lineHeight: LINE_HEIGHT_RATIO,
     fontWeight: 600,
   },
 };
